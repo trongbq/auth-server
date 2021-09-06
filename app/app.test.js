554c0785dabb9f600ad3e0956dc628bbc0114117
Trong Bui <trong.buiquoc@gmail.com>
2021-09-06 15:25:27 +0700

    Fix eslint issues

--- app/app.test.js
+++ app/app.test.js
@@ -1,10 +1,9 @@
-import { TestWatcher } from 'jest';
 import request from 'supertest';
 import app from './app';
 
 describe('Root path', () => {
-    test('it should response to GET', async () => {
-      const response = await request(app).get('/');
-      expect(response.statusCode).toBe(200);
-    });
+  test('it should response to GET', async () => {
+    const response = await request(app).get('/');
+    expect(response.statusCode).toBe(200);
+  });
 });
--- app/users/userInfo.js
+++ app/users/userInfo.js
@@ -1,10 +1,10 @@
 const userInfo = (req, res) => {
-    const a = {
-        username: 'Trong',
-        email: 'trong.buiquoc@gmail.co'
-    }
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(a));
+  const a = {
+    username: 'Trong',
+    email: 'trong.buiquoc@gmail.co',
+  };
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify(a));
 };
 
 export default userInfo;
